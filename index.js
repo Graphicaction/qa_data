7cd0803783a6464497684fa75f29f75bd18f7214
Rashmi Sharma <rksharma2679@gmail.com>
2020-04-10 16:39:21 -0400

    users

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 var mainContainer = document.getElementById("myData");
-
-// fetch('people.json')
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     appendData(data);
-//   })
-//   .catch(function (err) {
-//     console.log(err);
-//   });
